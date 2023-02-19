4f531cd8b0746146bfcfe5ee0dd7b325e112922d
Gustavo Inacio <gsinacio94@gmail.com>
2023-02-19 14:54:21 -0300

    Adds env usage to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3003,
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: env.PORT,
+    },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+  };
 });
